fd5a77a6c180236cd474b31486f3e9bae00de8c6
unknown <mickl0102@outlook.com>
2024-01-30 11:17:20 -0600

    Added permissions to each endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,32 +12,39 @@ import { UsersService } from './users.service'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
 import { JwtGuard } from 'src/auth/guard/jwt.guard'
+import { RolesGuard } from 'src/auth/guard/roles.guard'
+import { Roles } from 'src/auth/decorator/roles.decorator'
 
-@UseGuards(JwtGuard)
+@UseGuards(JwtGuard, RolesGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Roles('ADMIN')
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto)
   }
 
+  @Roles('ADMIN')
   @Get()
   findAll() {
     return this.usersService.findAll()
   }
 
+  @Roles('ADMIN')
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id)
   }
 
+  @Roles('ADMIN')
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto)
   }
 
+  @Roles('ADMIN')
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id)
