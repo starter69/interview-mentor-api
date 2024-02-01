594e387e4c70968e56c710494f9472f0d6735f3b
unknown <mickl0102@outlook.com>
2024-02-01 07:55:39 -0600

    Added duration

--- src/interviews/interviews.controller.ts
+++ src/interviews/interviews.controller.ts
@@ -11,6 +11,8 @@ import { InterviewsService } from './interviews.service'
 import { JwtGuard } from 'src/auth/guard/jwt.guard'
 import { CreateInterviewDto } from './dto/create-interview.dto'
 import { multerConfig } from 'src/utils/multer.config'
+import * as ffmpeg from 'fluent-ffmpeg'
+import * as ffprobeStatic from 'ffprobe-static'
 
 @Controller('interviews')
 export class InterviewsController {
@@ -23,6 +25,20 @@ export class InterviewsController {
     @UploadedFile() file: Express.Multer.File,
     @Body() createInterviewDto: CreateInterviewDto
   ) {
+    ffmpeg.setFfmpegPath(ffprobeStatic.path)
+    const filePath = `${file.destination}/${file.filename}`
+
+    const metadata = await new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(filePath, (err, metadata) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(metadata)
+        }
+      })
+    })
+    //@ts-ignore
+    createInterviewDto.duration = metadata.format.duration * 60
     await this.interviewService.createInterview(createInterviewDto, file.path)
   }
 }
