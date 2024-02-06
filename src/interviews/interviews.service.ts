ce59011f77716249143d28d5f347ab6fe5188743
Cedric <wangcedric272@gmail.com>
2024-02-06 03:50:39 -0500

    update interviews be to include user name

--- src/interviews/interviews.service.ts
+++ src/interviews/interviews.service.ts
@@ -23,12 +23,27 @@ export class InterviewsService {
   }
 
   findAll() {
-    return this.prisma.interviews.findMany()
+    return this.prisma.interviews.findMany({
+      include: {
+        user: {
+          select: {
+            name: true
+          }
+        }
+      }
+    })
   }
 
   async findOne(id: number) {
     return await this.prisma.interviews.findUnique({
-      where: { id }
+      where: { id },
+      include: {
+        user: {
+          select: {
+            name: true
+          }
+        }
+      }
     })
   }
 
@@ -36,6 +51,13 @@ export class InterviewsService {
     return await this.prisma.interviews.findMany({
       where: {
         user_id: userId
+      },
+      include: {
+        user: {
+          select: {
+            name: true
+          }
+        }
       }
     })
   }
